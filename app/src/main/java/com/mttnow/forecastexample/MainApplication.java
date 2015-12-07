e62ecfa27c314d10e2058d8642223057effb0f42
alahammad <ip7@live.de>
2015-12-07 15:01:44 +0300

    add crashlytics

--- app/src/main/java/com/mttnow/forecastexample/MainApplication.java
+++ app/src/main/java/com/mttnow/forecastexample/MainApplication.java
@@ -2,7 +2,10 @@ package com.mttnow.forecastexample;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+
 import cat.ereza.customactivityoncrash.CustomActivityOnCrash;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by alahammad on 12/4/15.
@@ -12,6 +15,7 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         CustomActivityOnCrash.install(this);
         CustomActivityOnCrash.setShowErrorDetails(false);
